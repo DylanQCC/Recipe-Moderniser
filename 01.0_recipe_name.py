e7fa54fbb8380d5b761f4a7413d25fb92d288e36
DylanQCC <65580179+DylanQCC@users.noreply.github.com>
2021-06-25 12:51:35 +1200

    Add files via upload

---
+++ 01.0_recipe_name.py
@@ -0,0 +1,30 @@
+# Asking user for recipe name
+# Blank check here
+
+def not_blank(question):
+    error = "Your recipe name has  numbers in it."
+    has_errors = ""
+
+    valid = False
+    while not valid:
+        response = input(question)
+
+        # look at each character in string and if it's a number, complain
+        for letter in response:
+            if letter.isdigit():
+                has_errors = "yes"
+                break
+
+        if response == "":
+            continue
+        elif has_errors != "":
+            print(error)
+            break
+        else:
+            return response
+
+
+# Main recipe question
+recipe_name = not_blank("Enter recipe name: ")
+
+print("You're making {}".format(recipe_name))
