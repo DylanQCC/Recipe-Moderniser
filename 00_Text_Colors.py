94a86e44b5e36494d7afea8a15d59e01e5960cbc
DylanQCC <65580179+DylanQCC@users.noreply.github.com>
2021-11-08 09:23:40 +1300

    Add files via upload

---
+++ 00_Text_Colors.py
@@ -0,0 +1,11 @@
+# Variables for colored text
+PURPLE_B = '\033[1;35m'  # Bold Purple Text
+GREEN = '\033[32m'  # Green Text
+YELLOW_B = '\033[1;33m'  # Bold Yellow Text
+RED = '\033[31m'  # Red Text
+RED_B = '\033[0;1;31m'  # Bold Red Text
+CYAN = '\033[1;36m'  # Cyan Text
+DEFAULT = '\033[m'  # Default Text
+WHITE_B = '\033[1m'  # Default Bold
+
+print(DEFAULT, "asdf")
